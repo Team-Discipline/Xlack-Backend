bfb9088d9dc95649fce467b2c5778acbf6376ced
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-07 17:28:44 +0900

    Refactoring.

--- app/model/crud/user.py
+++ app/model/crud/user.py
@@ -1,8 +1,8 @@
-from fastapi import Depends
-from ..database import get_db
-from sqlalchemy.orm import Session
-from .. import models
 import uuid
+
+from sqlalchemy.orm import Session
+
+from .. import models
 
 """
 This functions are not capable to authentication every actions.
@@ -13,12 +13,14 @@ async def create_user(db: Session,
                       github_id: str,
                       email: str,
                       name: str,
+                      thumbnail_url: str | None = None,
                       authorization_name: str = 'member',
                       refresh_token: str | None = None) -> models.User:
     """
     Create user into database.
 
-    :param github_id: github id provided by github.
+    :param thumbnail_url: Thumbnail image url from GitHub user info.
+    :param github_id: GitHub id provided by GitHub.
     :param email: email address.
     :param name: full name.
     :param authorization_name: name which is in `Authorization` in database.
@@ -31,6 +33,7 @@ async def create_user(db: Session,
                        email=email,
                        name=name,
                        authorization=authorization_name,
+                       thumbnail_url=thumbnail_url,
                        refresh_token=refresh_token)
     db.add(user)
     db.commit()
@@ -55,31 +58,41 @@ async def read_user(db: Session,
                         models.User.email,
                         models.User.name,
                         models.User.authorization,
-                        models.User.created_at) \
+                        models.User.created_at,
+                        models.User.thumbnail_url) \
             .filter(models.User.user_id == user_id).first()
     elif email is not None:
         return db.query(models.User.user_id,
                         models.User.email,
                         models.User.name,
                         models.User.authorization,
-                        models.User.created_at) \
+                        models.User.created_at,
+                        models.User.thumbnail_url) \
             .filter(models.User.email == email).first()
 
 
 async def read_users(db: Session) -> [models.User]:
-    return db.query(models.User).all()
+    return db.query(models.User.user_id,
+                    models.User.email,
+                    models.User.name,
+                    models.User.authorization,
+                    models.User.created_at,
+                    models.User.thumbnail_url).all()
 
 
 async def update_user(db: Session,
                       user_id: str,
                       email: str,
                       name: str,
+                      thumbnail_url: str | None = None,
                       refresh_token: str | None = None,
                       authorization_name: str = 'member') -> models.User:
     """
     Identify user only with **user_id**!!!
     Check authorization first!!!
 
+    :param authorization_name: Check GET `/authorization/all` first.
+    :param thumbnail_url: Thumbnail image url from GitHub user info you want to fix.
     :param user_id: Using when identifying user.
     :param email: Field to update.
     :param name: Field to update.
@@ -94,15 +107,10 @@ async def update_user(db: Session,
         .filter(models.User.user_id == user_id) \
         .update({'email': email,
                  'name': name,
-                 'authorization': authorization_name})
+                 'authorization': authorization_name,
+                 'refresh_token': refresh_token,
+                 'thumbnail_url': thumbnail_url})
     db.commit()
-
-    if refresh_token is not None:
-        user = db \
-            .query(models.User) \
-            .filter(models.User.user_id == user_id) \
-            .update({'refresh_token': refresh_token})
-        db.commit()
 
     return user
 
