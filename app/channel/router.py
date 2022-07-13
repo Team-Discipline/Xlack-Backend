a504b2dbb937d433c194d3e12bd78c43da831556
Jonghyeon Kim <85392646+ryankimjh00@users.noreply.github.com>
2022-07-13 23:38:48 +0900

    code refactoring

--- app/channel/router.py
+++ app/channel/router.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
-
 from app.model.crud.channel import create_channel, read_channel, read_channels, delete_channel, update_channel
 from app.model.database import get_db
+from app.model.models import Channel
 
 router = APIRouter(prefix='/channel', tags=['channel'])
 
 
-@router.post('/')
-async def channel_create(channel_name: str = "Untitled", db: Session = Depends(get_db)):
+@router.post('/', response_model=Channel)
+async def channel_create(channel: Channel, channel_name: str = "Untitled", db: Session = Depends(get_db)):
     # TODO: Connect to db.
     # TODO: 1. Input channel information to db.
 
@@ -21,7 +21,7 @@ async def channel_create(channel_name: str = "Untitled", db: Session = Depends(g
     }
 
 
-@router.get('/')
+@router.get('/', response_model=Channel)
 async def channel_read_by_name(channel_name: str, db: Session = Depends(get_db)):
     channel = await read_channel(db=db, channel_name=channel_name)
 
@@ -29,20 +29,20 @@ async def channel_read_by_name(channel_name: str, db: Session = Depends(get_db))
             'channel': channel}
 
 
-@router.get('/')
+@router.get('/', response_model=Channel)
 async def channel_read(db: Session = Depends(get_db)):
     all_channel = await read_channels(db=db)
     return {'all_channel': all_channel}
 
 
-@router.patch('/')
+@router.patch('/', response_model=Channel)
 async def channel_update(new_channel_name: str, old_channel_name: str, db: Session = Depends(get_db)):
     channel_updated = await update_channel(db=db, new_channel_name=new_channel_name, old_channel_name=old_channel_name)
     return {'channel updated': True,
             'channel': channel_updated}
 
 
-@router.delete('/')
+@router.delete('/', response_model=Channel)
 async def delete_channel(channel_name: str, db: Session = Depends(get_db)):
     if channel_name:
         raise HTTPException(status_code=404, detail="channel_name not found")
--- test/channel/channel_test.py
+++ test/channel/channel_test.py
@@ -1,51 +1,43 @@
 from fastapi.testclient import TestClient
-
+import unittest
 from app.channel.router import router
+from app.channel.router import channel_read_by_name
 
 client = TestClient(router)
 
 
-# class ChannelTest(unittest.TestCase):
-#
-#     @classmethod
-#     def setUpClass(cls) -> None:
-#         pass
-#
-#     @classmethod
-#     def tearDownClass(cls) -> None:
-#         pass
-#
-#     def setUp(self) -> None:
-#         pass
-#
-#     def tearDown(self) -> None:
-#         pass
+class ChannelTest(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
 
-def test_channel_read_by_name(channel_name):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.json() == {"channel_name": "test_channel_name"}
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
 
+    def setUp(self) -> None:
+        pass
 
-def test_channel_name_not_found():
-    response = client.get('/')
-    assert response.status_code == 404
-    assert response.json() == {"detail": "channel_name not found"}
+    def tearDown(self) -> None:
+        pass
 
+    def test_channel_read_by_name(self):
+        response = client.get('/channel')
+        assert response.status_code == 200
+        assert response.json() == {'channel_name': 'test_channel_name'}
 
-def test_invalid_keyword_error():
-    response = client.get('/')
-    assert response.status_code == 400
-    assert response.json() == {'detail': "invalid channel name requested"}
+    def test_channel_name_not_found(self):
+        response = client.get('/')
+        assert response.status_code == 404
+        assert response.json() == {"detail": "channel_name not found"}
 
+    def test_invalid_keyword_error(self):
+        response = client.get('/')
+        assert response.status_code == 400
+        assert response.json() == {'detail': "invalid channel name requested"}
 
-def test_db_connection_error():
-    response = client.get('/')
-    assert response.status_code == 400
-    assert response.json() == {'detail': 'db connection error'}
-
-
-def test_print():
-    response = client.get('/')
-    assert response.json() == {'print': 'print'}
+    def test_db_connection_error(self):
+        response = client.get('/')
+        assert response.status_code == 400
+        assert response.json() == {'detail': 'db connection error'}
