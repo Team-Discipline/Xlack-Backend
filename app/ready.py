1e580c6f50c0fbfb58a701caa5900f35fe9017b2
Aksidion Kreimben <aksidion@kreimben.com>
2022-07-17 14:15:48 +0900

    Unlock CORS.

--- app/ready.py
+++ app/ready.py
@@ -1,8 +1,25 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import app.model.database as database
+
+
+def ready_for_cors(app: FastAPI) -> FastAPI:
+    origins = [
+        '*'
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    return app
 
 
 def ready_for_db():
@@ -18,6 +35,8 @@ def ready_app() -> FastAPI:
         description='Furthermore Workspace.',
         version='0.1.0'
     )
+
+    app = ready_for_cors(app)
 
     is_debugging = os.getenv('IS_DEBUGGING')
     if not bool(is_debugging if is_debugging is not None else False):
