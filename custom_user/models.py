d25afcfbadd865faaca57f1aafce9baf77b0e6f7
Aksidion Kreimben <aksidion@kreimben.com>
2022-12-19 09:55:39 +0900

    Cherry-picking in features/profile

--- custom_user/models.py
+++ custom_user/models.py
@@ -7,6 +7,7 @@ class CustomUser(AbstractUser):
     display_name = models.CharField(max_length=50, default='')
     title = models.TextField(null=True, blank=True)
     phone_number = models.CharField(max_length=20, default='')
+    # 프로필 이미지 추가 해야 함.
 
     class Meta:
         verbose_name = 'CustomUser'
